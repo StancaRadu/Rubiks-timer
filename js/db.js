3ed3f751027bf9b5cdb16332302e36d03b05e69b
Stanca <radu.hakuo@gmail.com>
2023-10-05 13:54:03 +0100

    1

--- js/db.js
+++ js/db.js
@@ -1,4 +1,10 @@
-function dbfile() {
+import {
+  get,
+  set
+}from 'https://cdn.jsdelivr.net/npm/idb-keyval@5/dist/esm/index.js'
+
+
+export function dbfile() {
   console.log("db file callled");
     const request = window.indexedDB.open("database", 1);
     request.onerror = (event) => {
@@ -9,7 +15,7 @@ function dbfile() {
         console.log(db);
       };
 }
-function rubik_set(scramble, time){
+export function rubik_set(scramble, time){
   set(scramble, time)
     .then(()=>{
       console.log("yay");
--- js/global_vars.js
+++ js/global_vars.js
@@ -1,7 +1,3 @@
-import {
-    get,
-    set
-  }from 'https://cdn.jsdelivr.net/npm/idb-keyval@5/dist/esm/index.js'
 
 let db;
-let rubik_set
+console.log("moduke");
