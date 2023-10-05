7b17aacba44e31cc3707dc69fa2471c42a3ea312
Stanca <radu.hakuo@gmail.com>
2023-10-05 13:04:19 +0100

    1

--- js/db.js
+++ js/db.js
@@ -1,3 +1,8 @@
+import {
+  get,
+  set
+}from 'https://cdn.jsdelivr.net/npm/idb-keyval@5/dist/esm/index.js'
+
 function dbfile() {
     const request = window.indexedDB.open("database", 1);
     request.onerror = (event) => {
