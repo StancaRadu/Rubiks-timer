43dd4a7fe8488d4494914327c58829141a47e1ce
Stanca <radu.hakuo@gmail.com>
2023-10-11 05:15:38 +0100

    1

--- js/solver.js
+++ js/solver.js
@@ -1,15 +1,15 @@
 class Cube{
     constructor(){
-        this.corners = {
-            "cubl":{"t":"","b":"","":"l"},
-            "cubr":{"t":"","b":"","":"r"},
-            "cufl":{"t":"","f":"","":"l"},
-            "cufr":{"t":"","f":"","":"r"},
+        this.pieces = {
+            "cubl":{"y":"yellow","z":"blue","x":"red"},
+            "cubr":{"y":"yellow","z":"blue","x":"orange"},
+            "cufl":{"y":"yellow","z":"green","x":"red"},
+            "cufr":{"y":"yellow","z":"green","x":"orange"},
         
-            "cdbl":{"t":"","b":"","l":""},
-            "cdbr":{"t":"","b":"","r":""},
-            "cdfl":{"t":"","f":"","l":""},
-            "cdfr":{"t":"","f":"","r":""}
+            "cdbl":{"y":"white","z":"blue","x":"red"},
+            "cdbr":{"y":"white","z":"blue","x":"orange"},
+            "cdfl":{"y":"white","z":"green","x":"red"},
+            "cdfr":{"y":"white","z":"green","x":"orange"}
         }
         this.sequences = {
             "R":{
@@ -18,7 +18,7 @@ class Cube{
                     ["cufr","cdfr"],
                     ["cdfr","cdbr"],
                     ["cdbr","cubr"]],
-                "special": "r"
+                "special": "x"
             },
             "L":{
                 "sequence":[
@@ -26,7 +26,7 @@ class Cube{
                     ["cdbl","cdfl"],
                     ["cdfl","cufl"],
                     ["cufl","cubl"]],
-                "special": "l"
+                "special": "x"
             },
             "U":{
                 "sequence":[   
@@ -34,7 +34,7 @@ class Cube{
                     ["cubl","cufl"],
                     ["cufl","cufr"],
                     ["cufr","cubr"]],
-                "special": "t"
+                "special": "y"
             },
             "D":{
                 "sequence":[
@@ -42,7 +42,7 @@ class Cube{
                     ["cdfr","cdfl"],
                     ["cdfl","cdbl"],
                     ["cdbl","cdbr"]],
-                "special": "t"
+                "special": "y"
             },
             "F":{
                 "sequence":[
@@ -50,7 +50,7 @@ class Cube{
                     ["cufl","cdfl"],
                     ["cdfl","cdfr"],
                     ["cdfr","cufr"]],
-                "special": "f"
+                "special": "z"
             },
             "B":{
                 "sequence":[
@@ -58,21 +58,56 @@ class Cube{
                     ["cdbr","cdbl"],
                     ["cdbl","cubl"],
                     ["cubl","cubr"]],
-                "special": "b"
+                "special": "z"
             }
         }
 
     }
 
-    doMove(move = "R2") {
+    doMove(move = "R") {
         let modifier = 1
         if (move.length == 2 & !isNaN(move[1])){
             modifier = parseInt(move[1])
+        }else if(move.length == 2){
+            modifier = -1;
         }
         move = move[0].toUpperCase()
-        console.log({"modifier":modifier,"move":move});
-
+        let sequence = this.sequences[move]["sequence"]
+        let special = this.sequences[move]["special"]
+        let toSwap = [];
+        ["x","y","z"].forEach(element => {
+            if (element != special) toSwap.push(element);
+        });
+        let copy = {}
         
+        
+        for (let i = 0; i < modifier; i++) {
+            copy = {}
+            sequence.forEach(pair => {
+                copy = Object.assign(copy, this.pieces[pair[0]]);
+                // copy[pair[0]] = this.pieces[pair[0]]
+                console.log(copy);
+            });
+            sequence.forEach(pair => {
+                this.pieces[pair[0]][special] = copy[pair[1]][special]
+                this.pieces[pair[0]][toSwap[0]] = copy[pair[1]][toSwap[1]]
+                this.pieces[pair[0]][toSwap[1]] = copy[pair[1]][toSwap[0]]
+            });
+        }
+        
+
+        console.log(this.pieces);
+        console.log(copy);
+
+        console.log({
+            "modifier":modifier,
+            "move":move,
+            "sequence":sequence,
+            "special":special,
+            "permutations":toSwap,
+            "copy":copy
+        });
+
     }
 
 }
