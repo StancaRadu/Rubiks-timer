8c480cf81e8774d6e9c4a94b3898012c42448aab
Hakuo <radu.hakuo@gmail.com>
2024-02-06 16:04:40 +0000

    improved timer

--- js/timer.js
+++ js/timer.js
@@ -6,12 +6,18 @@ let htmlScramble = document.getElementById("scramble")
 let htmlInstructions = document.getElementById("instructions-span")
 
 
+// to be implemented
+wait_to_start = 1
+// 
+
 
 class Timer{
     constructor(){
         this.sTime = 0;
         this.nTime = 0;
         this.time = 0;
+        this.waited = false;
+        this.held = 0;
         this.ready = true;
         this.counting = false;
         this.interval;
@@ -38,9 +44,18 @@ class Timer{
 
         let minutes = Math.floor(time[0] / 60)
         let seconds = time[0] % 60
-        let miliseconds = time[1]
+        let miliseconds = parseInt(time[1])
+
+        let dminutes = minutes > 0 ? `${minutes} : ` : ""
+        let dseconds = seconds > 9 ? `${seconds}` : `0${seconds}`
+        let dmiliseconds = miliseconds > 9 ?  `${miliseconds}` : `0${miliseconds}`
+
+        htmlMinutes.innerHTML = dminutes
+        htmlSeconds.innerHTML = dseconds
+        htmlMiliseconds.innerHTML = dmiliseconds
+
         let solveTime = parseFloat(`${time[0]}.${time[1]}`)
-        console.log(solveTime);
+        console.log(minutes,seconds,miliseconds);
     }
     count(timer){
         timer.nTime = new Date().getTime();
@@ -59,19 +74,31 @@ let timer = new Timer
 
 
 document.addEventListener('keyup', (event) => {
-    if (!timer.counting && event.code == "Space" && timer.ready){
+    if (event.code == "Space" && !timer.counting && timer.ready && timer.waited){
         timer.ready = false;
         timer.start()
     }
     else if (!timer.ready){
         timer.ready = true;
     }
-});
-document.addEventListener('keydown', (event) => {
-    if (timer.counting && event.code == "Space"){
-        timer.stop()
+    if(event.code == "Space"){
+        timer.held = 0
     }
 });
+document.addEventListener('keydown', (event) => {
+    if (event.code == "Space" && !timer.counting){
+            if (!timer.held){
+                timer.held = new Date().getSeconds()
+            }
+            timer.waited = (new Date().getSeconds() - timer.held) >= wait_to_start ? true : false
+    }
+    if (timer.counting && event.code == "Space"){
+        timer.stop()
+        timer.waited = false
+    }
+});
+
+
 document.getElementById("new-scramble-button").addEventListener("click", () =>{
     // scramble = generateScramble(scrambleLength)
 })
