8cb8c46c219419e7d8227e4f1ded3e80ba93aab2
StancaRadu <radu.hakuo@gmail.com>
2024-02-07 16:39:08 +0000

    b

--- js/three.js
+++ js/three.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from '../node_modules/three/build/three.module'
 
 const canvas = document.getElementById("bg")
 let td = document.getElementById("td")
