8934450f63a77578ff1a1e77f51317fc7bf86a6a
Hakuo <radu.hakuo@gmail.com>
2024-03-07 12:31:36 +0200

    py code to create algs for json

--- src/tools/alg_writer.py
+++ src/tools/alg_writer.py
@@ -1,22 +1,23 @@
 while True:
     string = input()
-    string = string.replace("(","")
-    string = string.replace(")","")
-    string = string.replace(" ","")
+    moves = ["F", "B", "U", "D", "L", "R",
+             "f", "b", "u", "d", "l", "r"]
+    modifiers = ["'", "2"]
 
     alg_string = ''
     move = ''
     i = 0
     while i < len(string):
         x = string[i]
-        if i > 0 and x not in ["'", "2"]:
-            alg_string += '"' + move + '", '
-            move = ""
-        move += x
-        
+        if x in moves or x in modifiers:
+            if x in moves and move:
+                alg_string += f'"{move}", '
+                move = ""
+            move += x
         i += 1
-    alg_string += '"' + move + '"'
+        
+    alg_string += f'"{move}"'
 
     # alg_string = alg_string[
-    alg_string = "[ " + alg_string + " ]"
+    alg_string =  f"[ {alg_string} ]"
     print(alg_string)
